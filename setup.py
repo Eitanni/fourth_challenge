4ca45a38f1ce694d057c445c46d441b0185d411d
Eitan Nir <eitanir.is@gmail.com>
2021-05-24 16:25:46 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(name='4th_challenge',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/4th_challenge-run'],
+      scripts=['scripts/fourth_challenge-run'],
       zip_safe=False)
